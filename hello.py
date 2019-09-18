ecf669092e7884b2133fce82686d0b3691360b7e
claverru <36412653+claverru@users.noreply.github.com>
2019-09-18 18:18:50 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,50 +1,63 @@
 import random
-from deap import creator, base, tools, algorithms
+# import multiprocessing
+
 from string import printable
-from fuzzywuzzy import fuzz
+from deap import creator, base, tools, algorithms
 
-objective = "Hello world!"
 
-creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-creator.create("Individual", list, fitness=creator.FitnessMax)
-
-toolbox = base.Toolbox()
-
-toolbox.register("attr_bool", random.choice, printable)
-toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, n=len(objective))
-toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-
-def evaluate(individual):
-	# return fuzz.ratio(individual, objective),
+def evaluate(individual, obj):
 	val = 0
 	for i in range(len(individual)):
-		if individual[i] == objective[i]:
+		if individual[i] == obj[i]:
 			val += 1
 	return val, 
 
+
 def mutate(individual, indpb):
-	for i in range(len(individual)):
-		if random.random() <= indpb:
-			individual[i] = random.choice(printable)
+	individual[random.randrange(len(individual))] = random.choice(printable)
 	return individual,
 
 
-toolbox.register("evaluate", evaluate)
-toolbox.register("mate", tools.cxUniform, indpb=0.1)
-toolbox.register("mutate", mutate, indpb=0.2)
-toolbox.register("select", tools.selTournament, tournsize=3)
-
-population = toolbox.population(n=300)
-
-
-NGEN=1000
-for gen in range(NGEN):
-	offspring = algorithms.varAnd(population, toolbox, cxpb=0.5, mutpb=0.4)
-	fits = toolbox.map(toolbox.evaluate, offspring)
-	for fit, ind in zip(fits, offspring):
-		ind.fitness.values = fit
-	population = toolbox.select(offspring, k=len(population))
-	print("Generación {}/{}: {}".format(gen+1, NGEN, tools.selBest(population, k=1)[0]))
-top10 = tools.selBest(population, k=10)
+def generate(population, toolbox, ngen=1000):
+	for gen in range(ngen):
+		offspring = algorithms.varAnd(population, toolbox, cxpb=0.5, mutpb=0.4)
+		fits = toolbox.map(toolbox.evaluate, offspring)
+		for fit, ind in zip(fits, offspring):
+			ind.fitness.values = fit
+		population = toolbox.select(offspring, k=len(population))
+		best = tools.selBest(population, k=1)[0]
+		best_str = ''.join(best)
+		fit_per = best.fitness.values[0]/len(objective)
+		print("Generación {}/{}: {}\n{}".format(gen+1, ngen, fit_per, best_str))
+		if best_str == objective:
+			break
 
 
+if __name__ == '__main__':
+
+	objective = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+	
+	creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+	creator.create("Individual", list, fitness=creator.FitnessMax)
+	
+	toolbox = base.Toolbox()
+
+	# pool = multiprocessing.Pool()
+	# toolbox.register('map', pool.map)
+	
+	toolbox.register("printable", random.choice, printable)
+	toolbox.register(
+			"individual", 
+			tools.initRepeat, 
+			creator.Individual, 
+			toolbox.printable, 
+			n=len(objective))
+	
+	toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+	toolbox.register("evaluate", evaluate, objective)
+	toolbox.register("mate", tools.cxTwoPoint)
+	toolbox.register("mutate", mutate, indpb=0.2)
+	toolbox.register("select", tools.selTournament, tournsize=3)
+
+	generate(toolbox.population(n=300), toolbox)
